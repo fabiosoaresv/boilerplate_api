4a0fbc994e1b3024b4e37ca2835f3ee2d579a7fe
Fabio Soares <fabio.soares@cloudwalk.io>
2022-12-03 20:48:06 -0300

    Fix somethings

--- spec/jobs/hello_world_job_spec.rb
+++ spec/jobs/hello_world_job_spec.rb
@@ -8,8 +8,6 @@ RSpec.describe HelloWorldJob do
   end
 
   it 'should execute job async and return hello world' do
-    ActiveJob::Base.queue_adapter = :test
-    ActiveJob::Base.queue_adapter.perform_enqueued_jobs = true
     expect { HelloWorldJob.perform_later }.to have_performed_job(HelloWorldJob)
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-require_relative 'support/factory_bot'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
@@ -12,9 +11,13 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 end
+
+ActiveJob::Base.queue_adapter = :test
+ActiveJob::Base.queue_adapter.perform_enqueued_jobs = true
